f302f1c40031071afb937ead637d56c05fa398e0
Pratheek <pratheeksk70@gmail.com>
2024-01-17 19:05:37 +0530

    stylings almost done

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div className='message'>
+    <div className='message owner'>
       <div className="messageinfo">
         <img src='https://tse4.mm.bing.net/th?id=OIP.NqY3rNMnx2NXYo3KJfg43gAAAA&pid=Api&P=0&h=180' alt='' />
         <span>just now</span>
       </div>
       <div className="messageContent">
         <p>hello</p>
-        {/* <img src='https://tse4.mm.bing.net/th?id=OIP.NqY3rNMnx2NXYo3KJfg43gAAAA&pid=Api&P=0&h=180' alt=''/> */}
+        <img src='https://tse4.mm.bing.net/th?id=OIP.NqY3rNMnx2NXYo3KJfg43gAAAA&pid=Api&P=0&h=180' alt=''/>
       </div>
     </div>
   )
