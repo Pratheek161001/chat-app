eb083c5639e00af06858dfbdf56193c2cbed62ce
Pratheek <pratheeksk70@gmail.com>
2024-01-20 14:16:34 +0530

    inputjs

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,6 +3,11 @@ import img from '../img/img.png'
 import Attach from '../img/attach.png'
 import { ChatContext } from '../context/ChatContext'
 import { AuthContext } from '../context/Auth.Context'
+import {arrayUnion, updateDoc,doc,serverTimestamp, Timestamp} from "firebase/firestore";
+import { db,storage } from '../firebase'
+import { v4 as uuid } from 'uuid'
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+
 
 const Input = () => {
   const [text,settext]=useState('')
@@ -10,13 +15,60 @@ const Input = () => {
 
   const {currentUser}=useContext(AuthContext)
   const {data}=useContext(ChatContext);
-  const handleSend=()=>{
-    if(img){
+
+  const handleSend= async ()=>{
+    if(image){
+      const storageRef = ref(storage, uuid());
+
+      const uploadTask = uploadBytesResumable(storageRef, image);
+
+      uploadTask.on(
+        (error) => {
+          //TODO:Handle Error
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+            await updateDoc(doc(db, "chats", data.chatId), {
+              messages: arrayUnion({
+                id: uuid(),
+                text,
+                senderId: currentUser.uid,
+                date: Timestamp.now(),
+                img: downloadURL,
+              }),
+            });
+          });
+        }
+      );
 
     }
     else{
-      
+      await updateDoc(doc(db,'chats',data.chatId),{
+        messages:arrayUnion({
+          id:uuid,
+          text,
+          senderId:currentUser.uid,
+          Date:Timestamp.now(),
+        })
+      })
+
     }
+    await updateDoc(doc(db, "userChats", currentUser.uid), {
+      [data.chatId + ".lastMessage"]: {
+        text,
+      },
+      [data.chatId + ".date"]: serverTimestamp(),
+    });
+
+    await updateDoc(doc(db, "userChats", data.user.uid), {
+      [data.chatId + ".lastMessage"]: {
+        text,
+      },
+      [data.chatId + ".date"]: serverTimestamp(),
+    });
+
+    settext("");
+    setimg(null);
 
   }
 
